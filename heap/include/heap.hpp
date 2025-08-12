3ebf46c155baf0b02fd8848e076d811cc3b53f84
Jason <jjjjjason9@gmail.com>
2025-08-12 16:47:07 -0700

    feat(heap): start Heap class implementation (framework + partial methods)

--- heap/include/heap.hpp
+++ heap/include/heap.hpp
@@ -26,15 +26,184 @@ class Heap {
 public: 
     using size_type = std::size_t;
 
+    explicit Heap(Compare comp = Compare());
+    Heap(const T* ptr, size_type n, Compare comp = Compare());
+    Heap(std::initializer_list<T> init, Compare comp = Compare());
+    ~Heap();
+
+    Heap(const Heap& other);
+    Heap(Heap&& other) noexcept;
+    Heap& operator=(Heap other) noexcept;
+
+    const T& top() const;
+
+    template <class U>
+    void push(U&& value);  // append then sift_up
+
+    void pop();            // swap top with last, --size_, then sift_down
+    
+    void clear() noexcept;
+    bool empty() const noexcept;
+    size_type size() const noexcept;
+
 private:
-    size_type size_;
-    size_type capacity_;
-    T* data_;
-    Compare comp_;
+    void ensureCapacity();
+
+    void swap(Heap& other) noexcept;
+
+    void siftDown(size_type i); // returns true if moved downward
+    void siftUp(size_type i);
+    void buildHeap();           // bottom-up heapify, O(n)
+
+private:
+    size_type size_ = 0;
+    size_type capacity_ = 0;
+    T* data_ = nullptr;
+    Compare comp_{};
 
     static constexpr size_type kInitialCapacity = 4;
 };
 
+template<typename T, typename Compare>
+Heap<T, Compare>::Heap(Compare comp) 
+    : size_(0), 
+      capacity_(kInitialCapacity), 
+      data_(new T[kInitialCapacity]), 
+      comp_(std::move(comp)) 
+{}
+
+template<typename T, typename Compare>
+Heap<T, Compare>::Heap(const T* ptr, size_type n, Compare comp)
+    : size_(n), 
+      capacity_(n > kInitialCapacity ? k : kInitialCapacity), 
+      data_(new T[capacity_]),
+      comp_(std::move(comp)) {
+    for (size_type i = 0; i < n; ++i) {
+        data_[i] = ptr[i];
+    }
+    build_heap(); // O(n) bottom-up heapify
+}
+
+template <typename T, typename Compare>
+Heap<T, Compare>::Heap(std::initializer_list<T> init, Compare comp)
+    : size_(static_cast<size_type>(init.size())),
+      capacity_(size_ > kInitialCapacity ? size_ : kInitialCapacity),
+      data_(new T[capacity_]),
+      comp_(std::move(comp)) {
+    size_type i = 0;
+    for (const auto& x : init) data_[i++] = x;
+    build_heap(); // O(n)
+}
+
+template <typename T, typename Compare>
+Heap<T, Compare>::~Heap() {
+    delete[] data_;
+    data_ = nullptr;
+    size_ = 0;
+    capacity_ = 0;
+}
+
+template <typename T, typename Compare>
+Heap<T, Compare>::Heap(const Heap& other)
+    : size_(other.size_),
+      capacity_(other.capacity_),
+      data_(new T[capacity_]),
+      comp_(other.comp_){
+    for (size_type i = 0; i < size_; ++i) {
+        data_[i] = other.data_[i];
+    }
+}
+
+template <typename T, typename Compare>
+Heap<T, Compare>::Heap(Heap&& other) noexcept
+    : size_(other.size_), 
+      capacity_(other.capacity_),
+      data_(other.data_),
+      comp_(std::move(other.comp_)) {
+    other.size_ = 0;
+    other.capacity = 0;
+    other.data_ = nullptr;
+}
+
+template <typename T, typename Compare>
+Heap<T, Compare>& Heap<T, Compare>::operator=(Heap other) noexcept {
+    swap(other);
+    return *this;
+}
+
+template <typename T, typename Compare>
+void Heap<T, Compare>::ensureCapacity() {
+    if (size_ >= capacity_) {
+        size_type newCapacity = capacity_ == 0 ? kInitialCapacity : capacity_ * 4;
+        T* newData = new T[newCapacity];
+        for (size_type i = 0; i < size_; ++i) {
+            newData[i] = data_[i];
+        }
+        delete[] data_;
+        data_ = newData;
+        capacity_ = newCapacity;
+    }
+}
+
+template <typename T, typename Compare>
+void Heap<T, Compare>::swap(Heap& other) noexcept{
+    using std::swap;
+    swap(size_, other.size_);
+    swap(capacity_, other.capacity_);
+    swap(data_, other.data_);
+    swap(comp_, other.comp_);
+}
+
+template <typename T, typename Compare>
+void Heap<T, Compare>::siftDown(size_type i) {
+    while (true) {
+        size_type left  = 2 * i + 1;
+        size_type right = 2 * i + 2;
+        size_type best  = i;
+
+        // Compare for max-heap by default (Less<T> means greater value has priority)
+        if (left < size_ && comp_(data_[best], data_[left])) {
+            best = left;
+        }
+        if (right < size_ && comp_(data_[best], data_[right])) {
+            best = right;
+        }
+
+        if (best != i) {
+            std::swap(data_[i], data_[best]);
+            i = best;
+        } else {
+            break;
+        }
+    }
+}
+
+template<typename T, typename Compare>
+void Heap<T, Compare>::siftUp(size_type i) {
+    while (i > 0) {
+        size_type parent = (i - 1) / 2;
+        if (comp_(data_[parent], data_[i])) {
+            std::swap(data_[i], data_[parent]);
+            i = parent;
+        } else {
+            break;
+        }
+    }
+}
+
+template<typename T, typename Compare>
+void Heap<T, Compare>::buildHeap() {
+    if (size_ <= 1) {
+        return; 
+    }
+
+    // Traverse all non-leaf nodes in reverse order to build the heap.
+    // Use `i-- > 0` instead of `i >= 0` because `size_type` is unsigned;
+    // this avoids underflow when decrementing from 0.
+    for (size_type i = size_ / 2; i-- > 0; ) {
+        siftDown(i);
+    }
+}
 
 
 }
